17304e21c81017ecedeeca7c8816772a76a3577d
Marcelo Franca <marcelo.frneves@gmail.com>
2019-07-24 10:43:52 -0400

    Modules ambiente de prod

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,33 @@
-node 'homolog.uckg.us' {
+node 'lppsp1001.uckg.us' {
+  include confsbase
+}
+node 'balancer.uckg.us' {
+  include confsbase
+  include balancer
+  balancer::configure  { 'balancer.uckg.us': 
+  web1  => '192.168.0.34',
+  web2  => '192.168.0.35',
+  cache => '192.168.0.36',
+}
+}
+node 'webserver01.uckg.us' {
+  include confsbase
+  include webserver
+  webserver::configure  { 'webserver01.uckg.us': 
+  banco => '192.168.0.36',
+  cache => '192.168.0.36',
+}
+}
+node 'webserver02.uckg.us' {
+  include confsbase
+  include webserver
+  webserver::configure  { 'webserver02.uckg.us': 
+  banco => '192.168.0.36',
+  cache => '192.168.0.36',
+}
+}
+node 'database.uckg.us' {
   include confsbase
   include memcached
   include mysqlserver
-  include webserver
-  webserver::configure { 'homolog.uckg.us':
-  banco => '192.168.0.31',
-  cache => '192.168.0.31',
-  }
 }
